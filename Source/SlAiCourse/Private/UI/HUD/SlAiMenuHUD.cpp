f4716c23ee32bf8d096f59b6e2e7e5694ee6f6b4
Ando-game <adly129821@163.com>
2019-01-06 14:47:39 +0800

    source file

---
+++ Source/SlAiCourse/Private/UI/HUD/SlAiMenuHUD.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SlAiMenuHUD.h"
+#include "SSlAiMenuHUDWidget.h"
+#include "SlateBasics.h"
+
+ASlAiMenuHUD::ASlAiMenuHUD()
+{
+	
+}
