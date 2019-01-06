85e7a0b1431b4b16c3f096a93f1c0c33e3af24cb
Ando-game <adly129821@163.com>
2019-01-06 20:57:38 +0800

    brush crash

--- Source/SlAiCourse/Private/UI/Widget/SSlAiMenuHUDWidget.cpp
+++ Source/SlAiCourse/Private/UI/Widget/SSlAiMenuHUDWidget.cpp
@@ -11,12 +11,13 @@ BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
 void SSlAiMenuHUDWidget::Construct(const FArguments& InArgs)
 {
 	//获取编辑器中的MenuStyle，BPSlAiMenuStyle就是编辑器中样式的名字
-    MenuStyle = &SlAiStyle::Get().GetWidgetStyle<FSlAiMenuStyle>("BPSlAiMenuStyle");
+    //MenuStyle = &SlAiStyle::Get().GetWidgetStyle<FSlAiMenuStyle>("BPSlAiMenuStyle");
 
 	ChildSlot
 	[
-    	SNew(SImage)
-    	.Image(&MenuStyle->MenuHUDBackgroundBrush)//将笔刷用到image上
+    	SNew(SButton)
+		//.Image()
+    	//.Image(&MenuStyle->MenuHUDBackgroundBrush)//将笔刷用到image上
 	];
 }
 END_SLATE_FUNCTION_BUILD_OPTIMIZATION
